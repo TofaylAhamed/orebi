2b77475ae87822d860ea3dff20a9cee44d714dcb
few <tofaylahamed26438@gmail.com>
2025-08-13 14:16:55 +0600

    .

--- src/components/Layouts/BannerTwo.jsx
+++ src/components/Layouts/BannerTwo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Container from '../Container'
+import BannerTwo from '../../assets/BannerTwo.png'
 
 const BannerTwo = () => {
   return (
        <div className='pt-[30px] pb-[100px]'>
          <Link to={"/shop"}>
     <Container>
-        <div className="bg-[url(src/assets/BannerTwo.png)] h-[300px] bg-no-repeat bg-center bg-cover"></div>
+        <div style={{ backgroundImage: `url(${BannerTwo})`}} className="h-[300px] bg-no-repeat bg-center bg-cover"></div>
     </Container>
     </Link>
        </div>
